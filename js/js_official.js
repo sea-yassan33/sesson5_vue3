09f592d5e2ffb4ce61cdec9fc88a7abd4b61a905
sea-yassan33 <a.yasuda_tr51@me.com>
2023-03-04 16:16:35 +0900

    v-bind_sc

---
+++ js/js_official.js
@@ -0,0 +1,8 @@
+const app6 = Vue.createApp({
+  data: () => ({
+    message: 'Hello Vue.js!',
+    number: 100,
+    ok: true
+  })
+})
+app6.mount('#app6')
---
+++ js/v_bind_sc.js
@@ -0,0 +1,7 @@
+const app7 = Vue.createApp({
+  data: () => ({
+    message: 'Hello Vue.js!!',
+    url: 'https://www.google.com/'
+  })
+})
+app7.mount('#app7')
